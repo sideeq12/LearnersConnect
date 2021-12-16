e830d2f7ca487c4db3234655e8a37fcab282bf1a
sideeq12 <abdwaheed2018@gmail.com>
2021-12-16 20:29:29 +0000

    verification

--- frontend/src/components/verification/verify.js
+++ frontend/src/components/verification/verify.js
@@ -1,10 +1,20 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import "./verification.css"
+import VerifyNav from "./verifyNav";
 const Verify = ()=>{
     return (
-        <h2>
-            Verification tab
-        </h2>
+        <div className="verify">
+            <VerifyNav />
+            <h3>Kindly verify input the 4-digit pin sent to <span>sawaheed@oauife.edu.ng</span></h3>
+            <div className="pin">
+            <input type="tel" />
+            <input type="tel" />
+            <input type="tel" />
+            <input type="tel" />
+            </div>
+            <Link to="/dashboard" className="veri">Verify</Link>
+        </div>
     )
 }
 
